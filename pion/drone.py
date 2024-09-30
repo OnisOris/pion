3ed35aab892471b3a8f64d6ab26728860376923d
Michael <onisoris@yandex.ru>
2024-09-30 14:00:57 +0300

    Другая система количества отправки udp пакетов, также удалено вращение координат, добавлено описание всех функций

--- pion/drone.py
+++ pion/drone.py
@@ -14,36 +14,10 @@ def create_connection(connection_method, ip, port):
     return mav_socket
 
 
-def rot_z(vector: np.ndarray, angle: float | int) -> np.ndarray:
-    """
-    Функция вращает входные вектора вокруг оси z, заданной векторами-столбцами. Положительным вращением считается
-    по часовой стрелке при направлении оси к нам.
-    :param vector: Вращаемые вектор-строки, упакованные в матрицу nx3
-    :param angle: угол вращения, заданный в радианах
-    :return: np.ndarray
-    """
-    vector = np.array(vector)
-    rotate_z = np.array([[cos(angle), -sin(angle), 0],
-                         [sin(angle), cos(angle), 0],
-                         [0, 0, 1]])
-    if vector.shape == (3,) or vector.shape == (1, 3):
-
-        rot_vector = vector.dot(rotate_z)
-        return rot_vector
-    elif vector.shape == (2,) or vector.shape == (1, 2):
-        vector = np.hstack([vector, 0])
-        rot_vector = vector.dot(rotate_z)
-        return rot_vector[0:2]
-    elif vector.shape == (4,) or vector.shape == (1, 4):
-        rot_vector = vector[0:3].dot(rotate_z)
-        return np.hstack([rot_vector, vector[3]])
-
-
 class Pion:
     def __init__(self, ip: str = '10.1.100.114',
                  mavlink_port: int = 8001,
                  connection_method: str = 'udpout',
-                 rotate_xyz: int | float = 0,
                  combine_system: int=0):
         self.mavlink_socket = create_connection(connection_method=connection_method,
                                                 ip=ip, port=mavlink_port)
@@ -64,7 +38,6 @@ class Pion:
         self.threads = []
         # Задающая скорость target speed размером (4,), -> [vx, vy, vz, v_yaw], работает при запущенном потоке v_while
         self.t_speed = np.array([0, 0, 0, 0])
-        self.rotate_xyz = rotate_xyz
         self.period_send_speed = 0.05
         self.period_get_attitude = 0.05
         # Информация, включающая
@@ -74,6 +47,7 @@ class Pion:
         # Время создания экземпляра
         self.t0 = time.time()
         self.ip = ip
+        self.connection_lost = False
 
     @property
     def attitude(self) -> np.ndarray:
@@ -91,23 +65,23 @@ class Pion:
         self._send_command_long(command_name='ARM',
                                 command=mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
                                 param1=1,
-                                att=True)
+                                mavlink_send_number=self._mavlink_send_number)
 
     def disarm(self):
         self._send_command_long(command_name='DISARM',
                                 command=mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
                                 param1=0,
-                                att=True)
+                                mavlink_send_number=self._mavlink_send_number)
 
     def takeoff(self):
         self._send_command_long(command_name='TAKEOFF',
                                 command=mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-                                att=True)
+                                mavlink_send_number=self._mavlink_send_number)
 
     def land(self):
-            return self._send_command_long(command_name='LAND',
-                                           command=mavutil.mavlink.MAV_CMD_NAV_LAND,
-                                           att=True)
+        self._send_command_long(command_name='LAND',
+                                command=mavutil.mavlink.MAV_CMD_NAV_LAND,
+                                mavlink_send_number=self._mavlink_send_number)
 
     def goto(self, x: float | int,
              y: float | int,
@@ -118,11 +92,13 @@ class Pion:
         mask = 0b0000_10_0_111_111_000
         # ENU coordinates to NED coordinates
         x, y, z = y, x, -z
-        old_mavlink_send_number = self._mavlink_send_number
-        self._mavlink_send_number = 10
         self._send_position_target_local_ned(coordinate_system=mavutil.mavlink.MAV_FRAME_LOCAL_NED,
-                                             mask=mask, x=x, y=y, z=z, yaw=yaw)
-        self._mavlink_send_number = old_mavlink_send_number
+                                             mask=mask, 
+                                             x=x, 
+                                             y=y, 
+                                             z=z, 
+                                             yaw=yaw, 
+                                             mavlink_send_number=10)
 
     def send_speed(self, vx: float | int,
                    vy: float | int,
@@ -141,7 +117,12 @@ class Pion:
         # ENU coordinates to NED coordinates
         vx, vy, vz = vy, vx, -vz
         self._send_position_target_local_ned(coordinate_system=mavutil.mavlink.MAV_FRAME_LOCAL_NED,
-                                             mask=mask, vx=vx, vy=vy, vz=vz, yaw_rate=yaw_rate)
+                                             mask=mask,
+                                             vx=vx, 
+                                             vy=vy, 
+                                             vz=vz, 
+                                             yaw_rate=yaw_rate, 
+                                             mavlink_send_number=1)
 
     def _send_position_target_local_ned(self, coordinate_system, 
                                         mask=0b0000_11_0_111_111_111,
@@ -156,17 +137,70 @@ class Pion:
                                         afz: float | int = 0,
                                         yaw: float | int = 0,
                                         yaw_rate: float | int = 0,
-                                        target_system=None,
-                                        target_component=None) -> None:
+                                        target_system = None,
+                                        target_component = None,
+                                        mavlink_send_number = 1) -> None:
+        """
+        Функция отправляет команду MAVLink для установки целевой позиции
+        или скорости в локальной системе координат NED (North, East, Down). 
+        Параметры включают систему координат, маску для указания активных полей,
+        координаты (x, y, z), скорости (vx, vy, vz), ускорения и скорость поворота
+        по оси yaw.
+        :param coordinate_system: Система координат (например, NED).
+        :type coordinate_system: int
+        :param mask: Маска для включения или исключения определенных параметров.
+        :type mask: int
+        :param x: Координата x.
+        :type x: float | int
+        :param y: Координата y.
+        :type y: float | int
+        :param z: Координата z.
+        :type z: float | int
+        :param vx: Скорость по оси x.
+        :type vx: float | int
+        :param vy: Скорость по оси y.
+        :type vy: float | int
+        :param vz: Скорость по оси z.
+        :type vz: float | int
+        :param afx: Ускорение по оси x.
+        :type afx: float | int
+        :param afy: Ускорение по оси y.
+        :type afy: float | int
+        :param afz: Ускорение по оси z.
+        :type afz: float | int
+        :param yaw: Угол курса.
+        :type yaw: float | int
+        :param yaw_rate: Скорость изменения курса.
+        :type yaw_rate: float | int
+        :param target_system: Идентификатор целевой системы.
+        :type target_system: int, optional
+        :param target_component: Идентификатор целевого компонента.
+        :type target_component: int, optional
+        :param mavlink_send_number: Количество отправок команды.
+        :type mavlink_send_number: int
+        :return: None        
+        """
         if target_system is None:
             target_system = self.mavlink_socket.target_system
         if target_component is None:
             target_component = self.mavlink_socket.target_component
-        for _ in range(self._mavlink_send_number):
-            self.mavlink_socket.mav.set_position_target_local_ned_send(0, target_system, target_component,
+        for _ in range(mavlink_send_number):
+            self.mavlink_socket.mav.set_position_target_local_ned_send(0, 
+                                                                       target_system, 
+                                                                       target_component,
                                                                        coordinate_system,
-                                                                       mask, x, y, z, vx, vy, vz, afx, afy, afz,
-                                                                       yaw, yaw_rate)
+                                                                       mask, 
+                                                                       x, 
+                                                                       y, 
+                                                                       z, 
+                                                                       vx, 
+                                                                       vy, 
+                                                                       vz, 
+                                                                       afx, 
+                                                                       afy, 
+                                                                       afz,
+                                                                       yaw, 
+                                                                       yaw_rate)
 
     def _send_command_long(self,
                            command_name,
@@ -180,26 +214,62 @@ class Pion:
                            param7: float | int = 0,
                            target_system=None,
                            target_component=None, 
-                           att : bool = False) -> None:
+                           mavlink_send_number: int = 1) -> None:
+        """
+        Отправляет команду типа COMMAND_LONG через MAVLink.
+        :param command_name: Имя команды для логирования.
+        :type command_name: str
+        :param command: Команда MAVLink.
+        :type command: int
+        :param param1: Параметр 1 команды.
+        :type param1: float | int
+        :param param2: Параметр 2 команды.
+        :type param2: float | int
+        :param param3: Параметр 3 команды.
+        :type param3: float | int
+        :param param4: Параметр 4 команды.
+        :type param4: float | int
+        :param param5: Параметр 5 команды.
+        :type param5: float | int
+        :param param6: Параметр 6 команды.
+        :type param6: float | int
+        :param param7: Параметр 7 команды.
+        :type param7: float | int
+        :param target_system: Идентификатор целевой системы.
+        :type target_system: int, optional
+        :param target_component: Идентификатор целевого компонента.
+        :type target_component: int, optional
+        :param mavlink_send_number: Количество отправок команды.
+        :type mavlink_send_number: int
+        :return: None
+        """
         print(f"_send_command_long --> {command_name}")
         if target_system is None:
             target_system = self.mavlink_socket.target_system
         if target_component is None:
             target_component = self.mavlink_socket.target_component
         confirm = 0
-        old_mavlink_send_number = self._mavlink_send_number
-        if att:
-            self._mavlink_send_number = 10
         while True:
-            self.mavlink_socket.mav.command_long_send(target_system, target_component, command, confirm,
-                                                      param1, param2, param3, param4, param5, param6, param7)
+            self.mavlink_socket.mav.command_long_send(target_system,
+                                                      target_component, 
+                                                      command, 
+                                                      confirm,
+                                                      param1, 
+                                                      param2, 
+                                                      param3, 
+                                                      param4, 
+                                                      param5, 
+                                                      param6, 
+                                                      param7)
             confirm += 1
-            if confirm >= self._mavlink_send_number:
+            if confirm >= mavlink_send_number:
                 break
-        if att:
-            self._mavlink_send_number = old_mavlink_send_number
 
     def _send_heartbeat(self) -> None:
+        """
+        Отправляет сообщение HEARTBEAT для поддержания активного соединения с дроном.
+        :return: None
+        """
         self.mavlink_socket.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_GCS,
                                                mavutil.mavlink.MAV_AUTOPILOT_INVALID,
                                                0, 
@@ -208,10 +278,13 @@ class Pion:
         self._heartbeat_send_time = time.time()
 
     def _message_handler(self, combine_system: int=0) -> None:
-        """
-        Функция поддерживает отправку хартбитов на дрон, а также пишет актуальные координаты в attitude
-        Если combine_system = 0, то данные читаются только с локуса, если combine_system = 1, то данные читаются и с локуса и с оптики, 
-        если combine_system = 2, то данные читаются только с оптики
+
+        """   
+        Обрабатывает сообщения от дрона и отправляет heartbeat, обновляя координаты дрона.
+        :param combine_system: Определяет, с каких источников будут считываться данные:
+                            0 — только локус, 1 — локус и оптика, 2 — только оптика.
+        :type combine_system: int
+        :return: None
         """
         if combine_system == 0:
             while True:
@@ -252,16 +325,10 @@ class Pion:
         :type ampl: float | int
         :return: None
         """
-        if self.rotate_xyz == 0:
-            while self.speed_flag:
-                t_speed = self.t_speed * ampl
-                self.send_speed(t_speed[0], t_speed[1], t_speed[2], t_speed[3])
-                time.sleep(self.period_send_speed)
-        else:
-            v = ampl * rot_z(self.t_speed[0:3], self.rotate_xyz)
-            while self.speed_flag:
-                self.send_speed(v[0], v[1], v[2], self.t_speed[3])
-                time.sleep(self.period_send_speed)
+        while self.speed_flag:
+            t_speed = self.t_speed * ampl
+            self.send_speed(t_speed[0], t_speed[1], t_speed[2], t_speed[3])
+            time.sleep(self.period_send_speed)
 
     def set_v(self, ampl: float | int = 1) -> None:
         """
@@ -282,7 +349,8 @@ class Pion:
         self._send_command_long(command_name='REBOOT_BOARD',
                                 command=mavutil.mavlink.MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN,
                                 target_component=1,
-                                param1=1)
+                                param1=1,
+                                mavlink_send_number=self._mavlink_send_number)
 
     def set_attitude_check(self) -> None:
         """
@@ -313,3 +381,28 @@ class Pion:
         """
         self.speed_flag = False
         np.save(f'{path}{file_name}', self.trajectory[1:])
+
+    def led_control(self, led_id=255, r=0, g=0, b=0):
+        """
+        Управление светодиодами на дроне.
+
+        :param led_id: Идентификатор светодиода, который нужно управлять. Допустимые значения: 0, 1, 2, 3, 255.
+        255 — для управления всеми светодиодами одновременно.
+        :type led_id: int
+        :param r: Значение интенсивности красного канала (от 0 до 255).
+        :type r: int
+        :param g: Значение интенсивности зеленого канала (от 0 до 255).
+        :type g: int
+        :param b: Значение интенсивности синего канала (от 0 до 255).
+        :type b: int
+        :raises ValueError: Если переданы недопустимые значения для параметра led_id или для значений r, g, b.
+        :return: None
+        """
+        if led_id not in [255, 0, 1, 2, 3]:
+            raise ValueError(
+                f"Argument 'led_id' must have one of the following values: 0, 1, 2, 3, 255. But your value is {led_id}.")
+        if r < 0 or r > 255 or g < 0 or g > 255 or b < 0 or b > 255:
+            raise ValueError(
+                f"Arguments 'r', 'g', 'b' must have value in [0, 255]. But your values is r={r}, g={g}, b={b}.")
+        return self._send_command_long(command_name='LED', command=mavutil.mavlink.MAV_CMD_USER_1,
+                                       param1=led_id, param2=r, param3=g, param4=b)
