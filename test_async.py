eb3209b271384dda5b8064bddca2feca5b19982f
Michael <onisoris@yandex.ru>
2024-10-25 10:24:23 +0300

    Файл для тестирования Apion

---
+++ test_async.py
@@ -0,0 +1,45 @@
+from pion import Apion
+import sys
+import asyncio
+import numpy as np
+
+args = sys.argv
+
+number_drone = sys.argv[1]
+drone = Apion(ip=f"10.1.100.{number_drone}", mavlink_port=5656)
+
+
+async def main():
+    if '-c' in args:
+        while True:
+            print(f"Attitude: {drone.attitude}")
+            await asyncio.sleep(0.1)  # Асинхронная задержка с небольшим интервалом
+    else:
+        print("---")
+        drone.land()
+        drone.arm()
+        drone.takeoff()
+        await asyncio.sleep(5)
+
+        print(f"Initial t_speed: {drone.t_speed}")
+
+        # Запуск асинхронного цикла отправки скорости
+        v_while_task = asyncio.create_task(drone.set_v_async())
+
+        # Попробуем двигать дрон
+        await drone.goto_from_outside(int(args[2]), int(args[3]), int(args[4]), 0)
+
+        print(f"After goto_from_outside t_speed: {drone.t_speed}")
+
+        await asyncio.sleep(10)
+        drone.land()
+
+        # Остановим все задачи
+        drone.stop()
+        v_while_task.cancel()  # Отменим задачу, чтобы остановить цикл
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
