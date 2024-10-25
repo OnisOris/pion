f06e23fa36888ee79ceef67cc7b366a3aa31e5ad
Michael <onisoris@yandex.ru>
2024-10-25 14:47:42 +0300

    Тестовый файл для запуска Pion

---
+++ test.py
@@ -0,0 +1,25 @@
+from pion import Pion
+import sys
+import time
+
+args = sys.argv
+
+number_drone = sys.argv[1]
+drone = Pion(ip=f"10.1.100.{number_drone}", mavlink_port=5656)
+drone.speed_flag = False
+if '-c' in args:
+    while True:
+        print(drone.attitude)
+        time.sleep(0)
+else:
+    print("---")
+    drone.land()
+    drone.arm()
+    drone.takeoff()
+    time.sleep(5)
+    drone.set_v()
+    drone.goto_from_outside(float(args[2]), float(args[3]), float(args[4]), float(args[5]))
+    time.sleep(10)
+    drone.land()
+    drone.stop()
+
