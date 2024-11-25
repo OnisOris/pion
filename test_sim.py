aec252b84282a3a8117fd7408109596cfe1cc04e
Michael <onisoris@yandex.ru>
2024-11-26 00:45:44 +0300

    Пример использования симуляции

--- test_sim.py
+++ test_sim.py
@@ -4,26 +4,20 @@ import time
 
 args = sys.argv
 
-drone = Spion(ip=f"10.1.100.200", mavlink_port=5656, mass=0.3, dt=0.001)
-
-drone.max_speed = 1000
+drone = Spion(ip=f"10.1.100.200", mavlink_port=5656, mass=0.3, dt=0.05)
 print("---")
-drone.set_v()
-
 print("Main arm ------------<")
-
 drone.arm()
-
 print("takeoff -----------------<")
-
 drone.takeoff()
+time.sleep(5)
 
 print("goto --------------<")
 
 drone.goto(20, 1, 1, 0)
 print("land ----------------<")
 
-drone.land()
+# drone.land()
 
 print("stop ----------------<")
 
