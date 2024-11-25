f2575170329b416ad448e5fe3a54b31933335b0b
Michael <onisoris@yandex.ru>
2024-11-25 21:27:44 +0300

    Аннотации для векторов размером 2, 3, 6, 7

---
+++ pion/annotation.py
@@ -0,0 +1,7 @@
+from numpy.typing import NDArray
+from typing import Annotated, Any
+# Аннотации для векторов различных размеров
+Array7 = Annotated[NDArray[Any], (7,)]
+Array6 = Annotated[NDArray[Any], (6,)] 
+Array3 = Annotated[NDArray[Any], (3,)]
+Array2 = Annotated[NDArray[Any], (2,)]
