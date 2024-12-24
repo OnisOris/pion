f43c6f2bedad2a7cd53748ae2863639fca3847ca
Michael <onisoris@yandex.ru>
2024-12-24 14:53:00 +0300

    Аннотация на массива длинной 4

--- pion/annotation.py
+++ pion/annotation.py
@@ -3,5 +3,6 @@ from typing import Annotated, Any
 # Аннотации для векторов различных размеров
 Array7 = Annotated[NDArray[Any], (7,)]
 Array6 = Annotated[NDArray[Any], (6,)] 
+Array4 = Annotated[NDArray[Any], (4,)]
 Array3 = Annotated[NDArray[Any], (3,)]
 Array2 = Annotated[NDArray[Any], (2,)]
