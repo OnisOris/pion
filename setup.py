ef7f11e574f1ff73b22df4f1c02237a5ce2e83bf
Egor <egor.goncharov.osia@mail.ru>
2025-03-11 22:20:24 +0300

    :wrench: Update setup.py and pyproject.toml
    Для автоматической сборки изменил
    структуру проекта под src-layout

--- setup.py
+++ setup.py
@@ -1,40 +1,17 @@
-from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
+from setuptools import setup, find_packages # , Extension
 import numpy as np
-
-def readme():
-    with open('README.md', 'r', encoding='utf-8') as f:
-        return f.read()
+from Cython.Distutils.extension import Extension
 
 # Cython-модуль, который нужно скомпилировать
 extensions = [
     Extension(
-        "pion.cython_pid",
-        ["pion/cython_pid.pyx"],
-        include_dirs=[np.get_include()]
+        name="pion.cython_pid",
+        sources=["src/pion/cython_pid.pyx"],
+        include_dirs=[np.get_include()],
+        cython_directives={"language_level": "3str"},
     ),
 ]
 
 setup(
-    name='pion',
-    version='0.0.1',
-    ext_modules=cythonize(extensions),
-    author='OnisOris',
-    author_email='onisoris@yandex.ru',
-    description='This module is needed to control drones.',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/OnisOris/pion',
-    packages=find_packages(),
-    install_requires=['numpy', 'pymavlink', 'rich', 'paramiko', 'protobuf', 'pyserial'],
-    classifiers=[
-        'Programming Language :: Python :: 3.12',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: OS Independent'
-    ],
-    keywords='drone quadrotor quadcopter control',
-    project_urls={
-        'GitHub': 'https://github.com/OnisOris/pion'
-    },
-    python_requires='>=3.9'
+    ext_modules=extensions,
 )
