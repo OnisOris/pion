51c6b426b98766354ecfa1e86a88dd1381cdeeb6
Michael <onisoris@yandex.ru>
2025-03-20 01:22:45 +0300

    Дизарм дронов после land

--- scripts/test.py
+++ scripts/test.py
@@ -17,6 +17,8 @@ if '-c' in args:
 elif '-l' in args:
     drone.led_control(255, 0, 0, 0)
     drone.land()
+    time.sleep(10)
+    drone.disarm()
 
 elif '-d' in args:
     drone.led_control(255, 255, 0, 0)
