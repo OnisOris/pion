a165cc7235e0a1ca83dfda9ed0306ae1fa37b37c
Michael <onisoris@yandex.ru>
2025-04-18 08:53:24 +0300

    Убрано взятие локального ip

--- src/swarm_server/scripts/server_sim.py
+++ src/swarm_server/scripts/server_sim.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import argparse
 import time
+
 import numpy as np
 
 from pion.spion import Spion
-from pionfunc.functions import get_local_ip
 from swarm_server import SwarmCommunicator
 
 # from .params import params
@@ -25,6 +25,7 @@ params = {
     "unstable_radius": 1.5,
 }
 
+
 def main():
     parser = argparse.ArgumentParser(description="Симуляция члена роя")
     parser.add_argument(
@@ -39,7 +40,7 @@ def main():
         nargs=4,  # ожидаем 4 значения: x, y, z, yaw
         metavar=("x", "y", "z", "yaw"),
         default=[0.0, 0.0, 0.0, 0.0],
-        help="Начальные координаты (x, y, z) и угол поворота (yaw)"
+        help="Начальные координаты (x, y, z) и угол поворота (yaw)",
     )
     args = parser.parse_args()
 
@@ -49,12 +50,11 @@ def main():
     attitude = np.array([0, 0, yaw, 0, 0, 0])
     args = parser.parse_args()
     # Получаем локальный IP-адрес
-    ip = get_local_ip()
     drone = Spion(
         ip="/dev/ttyS0",
         mavlink_port=230400,
         connection_method="serial",
-        name=f"Drone-{ip}-{args.id}",
+        name=f"Drone-localhost-{args.id}",
         dt=0.1,
         logger=False,
         max_speed=0.5,
@@ -72,7 +72,7 @@ def main():
         unique_id=args.id,
     )
     swarm_comm.start()
-    print(f"SwarmCommunicator запущен для {drone.name} с IP {ip}")
+    print(f"SwarmCommunicator запущен для {drone.name}")
 
     try:
         while True:
