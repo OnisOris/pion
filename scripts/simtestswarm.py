a9b2766172ef8e0b2892dafae623ca51cbbfe0b8
Michael <onisoris@yandex.ru>
2025-04-18 09:58:56 +0300

    New scripts

---
+++ scripts/simtestswarm.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from swarm_server.swarmsim import SwarmSim, create_objects_Point_yaw
+
+if __name__ == "__main__":
+    number_of_objects = 16
+    swarmsim = SwarmSim(
+        create_objects_Point_yaw(
+            int(np.sqrt(number_of_objects)), [-5, 5], [-5, 5]
+        ),
+        logger=True,
+    )
+    t_speed = np.ones((number_of_objects, 4))
+    swarmsim.t_speed = t_speed
+    swarmsim.start_simulation_while()
